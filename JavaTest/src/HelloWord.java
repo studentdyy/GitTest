2a315edc66416da63fd2462c920975c75536b0a6
studentdyy <t08069a@163.com>
2020-09-25 19:36:39 +0800

    simple example

---
+++ JavaTest/src/HelloWord.java
@@ -0,0 +1,9 @@
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.print("hello word");
+
+    }
+    public void nameRule(){
+        System.out.print("首个单词小写后面字母要大写，类的话都要大写");
+    }
+}
