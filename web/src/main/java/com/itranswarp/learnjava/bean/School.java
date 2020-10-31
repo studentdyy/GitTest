696058823ba6d405cb7dbcccc8c0b60c2955063c
studentdyy <t08069a@163.com>
2020-10-31 14:39:19 +0800

    perpare for MVC

--- web/src/main/java/com/itranswarp/learnjava/bean/School.java
+++ web/src/main/java/com/itranswarp/learnjava/bean/School.java
@@ -1,8 +1,13 @@
-package com.itranswarp.learnjava.javabean;
+package com.itranswarp.learnjava.bean;
 
 public class School {
     public String name;
     public String address;
+
+    public School(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
 
     public String getName() {
         return name;
--- web/src/main/java/com/itranswarp/learnjava/bean/User.java
+++ web/src/main/java/com/itranswarp/learnjava/bean/User.java
@@ -1,9 +1,15 @@
-package com.itranswarp.learnjava.javabean;
+package com.itranswarp.learnjava.bean;
 
 public class User {
     public long id;
     public String name;
     public School school;
+
+    public User(long id, String name, School school) {
+        this.id = id;
+        this.name = name;
+        this.school = school;
+    }
 
     public long getId() {
         return id;
